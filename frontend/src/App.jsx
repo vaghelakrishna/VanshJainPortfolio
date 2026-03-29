c0b72adedfdd8b2d2d41738c475a9b56df022740
vaghelakrishna <vaghelakrishna523@gmail.com>
2026-03-29 22:10:49 +0530

    import BlogPage component in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import AboutPage from './pages/AboutPage';
 import HomePage from './pages/HomePage';
 import Footer from './ui/Footer';
 import ContactPage from './pages/ContactPage';
+import BlogPage from './pages/BlogPage';
 // Placeholder components for the ones you haven't imported yet
 const SessionsPage = () => <h1>Speaker Sessions</h1>;
 const NotFound = () => <h1>404 - Page Not Found</h1>;
