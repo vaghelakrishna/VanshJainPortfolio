63dbd7ddb1e6691eb9354a6b5bf7341683dc2e41
vaghelakrishna <vaghelakrishna523@gmail.com>
2026-03-31 19:05:02 +0530

    add subsribe

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const Contact = require('./Models/Contact'); // Aapka model
+const Subscriber = require('./Models/Subscriber');
 
 const app = express();
 
@@ -42,6 +43,19 @@ app.post('/api/contact', async (req, res) => {
   }
 });
 
+app.post('/api/subscribe', async (req, res) => {
+  try {
+    const { email } = req.body;
+    const newSub = new Subscriber({ email });
+    await newSub.save();
+    res.status(201).json({ success: true, message: "Subscribed successfully!" });
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.status(400).json({ success: false, message: "Already subscribed!" });
+    }
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
